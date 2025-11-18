23d6be69816c2a862133c856f9f4b2529dcaa934
Takashi Takebayashi <changesworlds@gmail.com>
2025-11-18 16:39:30 +0900

    feat(prettier-config): export base Prettier configuration from src/index.ts

---
+++ packages/prettier-config/src/index.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'prettier'
+
+export const prettierConfig: Config = {
+  arrowParens: 'avoid',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  jsxSingleQuote: true,
+  printWidth: 80,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  useTabs: false
+}
